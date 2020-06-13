3f89e7b4a1270d3e169a669e6efb8deac10e9a32
Jay Liu <snowgy920@hotmail.com>
2020-06-13 21:34:28 +0800

    use js-sticky-sidebar comp

---
+++ app/code/Smartwave/Porto/view/frontend/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+            'porto/resizesensor' : 'Smartwave_Porto/js/resize-sensor',
+            'porto/stickysidebar' : 'Smartwave_Porto/js/sticky-sidebar.min',
+        }
+    }
+};
